266b8bd5a3dc67ec6bf08d670db8d9ea52fa353d
Trebor Shankle <trebor.shankle@gmail.com>
2011-04-19 00:50:47 +0000

    Added features for story 2 (identical map checker); got to a failing test.
    --Trebor Shankle and Jake Householder

--- features/step_definitions/core_steps.rb
+++ features/step_definitions/core_steps.rb
@@ -1,12 +1,26 @@
+INPUT_PATH = "features/input_files/"
+
+Given /^key "([^"]*)" and input "([^"]*)"$/ do |key, input|
+  @key_file_name = INPUT_PATH + key
+  @input_file_name = INPUT_PATH + input
+  
+  if !File.readable? @key_file_name
+    pending "File #{@key_file_name} does not exist!"
+  elsif !File.readable? @input_file_name
+    pending "File #{@input_file_name} does not exist!"
+  end
+end
+
 Given /^the input file is "([^"]*)"$/ do |file_name|
-  @input_file_name = "features/input_files/" + file_name
+  @input_file_name = INPUT_PATH + file_name
+  
   if !File.readable? @input_file_name
     pending "File #{@input_file_name} does not exist!"
   end
 end
 
 Given /^an input file that does not exist$/ do
-  @input_file_name = "features/input_files/does_not_exist.cxl"
+  @input_file_name = INPUT_PATH + "does_not_exist.cxl"
   if File.readable? @input_file_name
     pending "File #{@input_file_name} does exist, but should not!"
   end
@@ -14,7 +28,7 @@ end
 
 When /^cmap-eval is executed$/ do
   # Get a string containing the output of cmap-eval, separated by new-lines.
-  @output = `ruby src/cmap-eval.rb #{@input_file_name}`
+  @output = `ruby src/cmap-eval.rb #{@key_file_name} #{@input_file_name}`
 end
 
 Then /^it will display "([^"]*)"$/ do |expected_output|
@@ -26,4 +40,15 @@ Then /^the notification should read:$/ do |notification_table|
   actual_table = [@output.split("\n")].transpose
   
   notification_table.diff!(actual_table)
+end
+
+Then /^the notification should contain:$/ do |notification_table|
+  #Construct an array of arrays of strings to compare to our actual table.
+  actual_table = [@output.split("\n")].transpose
+  
+  notification_table.raw.each do |expected|
+    if !actual_table.include? expected
+      raise %{Notification did not contain "#{expected}".}
+    end
+  end
 end
