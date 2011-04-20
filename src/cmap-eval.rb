00abe72077c9c2a8a59d000d614086e4d2929ca2
Jake Householder <householder.33@gmail.com>
2011-04-20 22:06:28 +0000

    Removed story2.feature and its implementation in cmap-eval.
    --Jake Householder and Trebor Shankle and Kyle Callicoat

--- src/cmap-eval.rb
+++ src/cmap-eval.rb
@@ -26,23 +26,4 @@ elsif ARGV.size == 1
   rescue CMap::Error=> error
     puts error.message
   end
-else
-  # Parse the maps.
-  begin
-    if !File.readable? ARGV[KEY_FILE_INDEX]
-      puts %{Error: The provided file #{ARGV[KEY_FILE_INDEX]} does not exist.}
-    elsif !File.readable? ARGV[INPUT_FILE_INDEX]
-      puts %{Error: The provided file #{ARGV[INPUT_FILE_INDEX]} does not exist.}
-    else
-      # Parse the supplied files.
-      key_cmap = CMap::CMap.new Nokogiri::XML File.read ARGV[KEY_FILE_INDEX]
-      input_cmap = CMap::CMap.new Nokogiri::XML File.read ARGV[INPUT_FILE_INDEX]
-      
-      if key_cmap.equal? input_cmap
-        puts %{COMPARISON: Identical.}
-      else
-        puts %{COMPARISON: Not identical.}
-      end
-    end
-  end
 end
