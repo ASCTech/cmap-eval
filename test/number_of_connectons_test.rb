2ca9d7a7914b6b5e9d9d597b88e553144f514d2e
Kyle Callicoat <zspacekcc@gmail.com>
2011-05-16 23:47:41 +0000

    Kyle Callicoat: Unit tests: number_of_connections_test

--- test/number_of_connectons_test.rb
+++ test/number_of_connectons_test.rb
@@ -5,25 +5,25 @@ require "nokogiri"
 require "pp"
 
 include Nokogiri::XML
-  class EdgeLabelBetweenTest < Test::Unit::TestCase
-    def test_no_edges_between
+  class NumberOfConnectionsTest < Test::Unit::TestCase
+    def test_no_edges
       test = Builder.new do |xml|
         xml.cmap("xmlns" => "http://cmap.ihmc.us/xml/cmap/") {
           xml.parent.namespace = xml.parent.namespace_definitions.first
           xml.map {
             xml.send(:"concept-list") {
-              xml.concept("label" => "node1", "id" => "1JCK0VTLG-1FT6YS1-FV")
-              xml.concept("label" => "node2", "id" => "1JCK0VTLG-1FT6YS2-FV")
-              xml.concept("label" => "node3", "id" => "1JCK0VTLG-1FT6YS3-FV")
+              xml.concept("label" => "node1", "id" => "idnode1")
+              xml.concept("label" => "node2", "id" => "idnode2")
+              xml.concept("label" => "node3", "id" => "idnode3")
             }
           }
         }
       end   
         cmap = CMap::CMap.new(test.doc)
-        assert_equal([],cmap.edges_between("node1","node2"))
+        assert_equal(0, cmap.number_of_distinct_connections)
     end
     
-    def test_1_edge_between
+    def test_1_edge
       test = Builder.new do |xml|
         xml.cmap("xmlns" => "http://cmap.ihmc.us/xml/cmap/") {
           xml.parent.namespace = xml.parent.namespace_definitions.first
@@ -44,13 +44,10 @@ include Nokogiri::XML
         }
       end
       cmap = CMap::CMap.new(test.doc)
-      assert_equal(["edge1"],cmap.edges_between("node1","node2"))
-      
-      #test to make sure they don't work in reverse (edges are one way)
-      assert_not_equal(["edge1"],cmap.edges_between("node2","node1"))
+      assert_equal(1, cmap.number_of_distinct_connections)
     end
     
-    def test_mult_edge_between
+    def test_mult_edge
       test = Builder.new do |xml|
         xml.cmap("xmlns" => "http://cmap.ihmc.us/xml/cmap/") {
           xml.parent.namespace = xml.parent.namespace_definitions.first
@@ -74,46 +71,10 @@ include Nokogiri::XML
         }
       end
       cmap = CMap::CMap.new(test.doc)
-      assert_equal(["edge1", "edge2"],cmap.edges_between("node1","node2"))
-      
-      #test to make sure they don't work in reverse (edges are one way)
-      assert_not_equal(["edge1", "edge2"],cmap.edges_between("node2","node1"))
+      assert_equal(2, cmap.number_of_distinct_connections)
     end
     
-    def test_several_nodes
-      test = Builder.new do |xml|
-        xml.cmap("xmlns" => "http://cmap.ihmc.us/xml/cmap/") {
-          xml.parent.namespace = xml.parent.namespace_definitions.first
-          xml.map {
-            xml.send(:"concept-list") {
-              xml.concept("label" => "node1", "id" => "idnode1")
-              xml.concept("label" => "node2", "id" => "idnode2")
-              xml.concept("label" => "node3", "id" => "idnode3")
-            }
-            xml.send(:"linking-phrase-list") {
-              xml.send(:"linking-phrase", "label" => "edge1", "id" => "idedge1")
-              xml.send(:"linking-phrase", "label" => "edge2", "id" => "idedge2")
-            }
-            xml.send(:"connection-list") {
-              xml.connection("id" => "node1toedge1", "from-id" => "idnode1", "to-id" => "idedge1")
-              xml.connection("id" => "edge1tonode2", "from-id" => "idedge1", "to-id" => "idnode2")
-              xml.connection("id" => "node2toedge2", "from-id" => "idnode2", "to-id" => "idedge2")
-              xml.connection("id" => "edge2tonode3", "from-id" => "idedge2", "to-id" => "idnode3")
-            }
-          }
-        }
-      end
-      cmap = CMap::CMap.new(test.doc)
-      assert_equal(["edge1"],cmap.edges_between("node1","node2"))
-      assert_equal(["edge2"],cmap.edges_between("node2","node3"))
-      
-      #test to make sure they don't work in reverse (edges are one way)
-      
-      assert_not_equal(["edge1"],cmap.edges_between("node2","node1"))
-      assert_not_equal(["edge2"],cmap.edges_between("node3","node2"))
-    end
-    
-    def test_circular_nodes
+    def test_circular_edges
       test = Builder.new do |xml|
         xml.cmap("xmlns" => "http://cmap.ihmc.us/xml/cmap/") {
           xml.parent.namespace = xml.parent.namespace_definitions.first
@@ -137,12 +98,6 @@ include Nokogiri::XML
         }
       end
       cmap = CMap::CMap.new(test.doc)
-      assert_equal(["edge1"],cmap.edges_between("node1","node2"))
-      assert_equal(["edge2"],cmap.edges_between("node2","node1"))
-      
-      #test to make sure they don't work in reverse (edges are one way)
-      
-      assert_not_equal(["edge1"],cmap.edges_between("node2","node1"))
-      assert_not_equal(["edge2"],cmap.edges_between("node1","node2"))
+      assert_equal(2,cmap.number_of_distinct_connections, "Circular edges break the method.")
     end
   end
