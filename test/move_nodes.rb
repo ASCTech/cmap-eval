0b636935eed15b79f82f2bf9703f7f44965f8f47
Kyle Callicoat <zspacekcc@gmail.com>
2011-05-25 00:42:07 +0000

    Kyle Callicoat: Unit tests

--- test/move_nodes.rb
+++ test/move_nodes.rb
@@ -99,7 +99,46 @@ class MoveNodesTest < Test::Unit::TestCase
   end
   
   #This trys to move test nodes with no defined location into the key location
-  def test_missing_appear_mult_node
+  def test_no_pos_mult_node
+    test = Builder.new do |xml|
+        xml.cmap("xmlns" => "http://cmap.ihmc.us/xml/cmap/") {
+          xml.map {
+            xml.send(:"concept-list") {
+              xml.concept("label" => "node1", "id" => "idnode1")
+              xml.concept("label" => "node2", "id" => "idnode2")
+            }
+            xml.send(:"concept-appearance-list") {
+              xml.send(:"concept-appearance", "id" => "idnode1")
+              xml.send(:"concept-appearance", "id" => "idnode2")
+            }
+          }
+        }
+    end
+    
+    correct = Builder.new do |xml|
+        xml.cmap("xmlns" => "http://cmap.ihmc.us/xml/cmap/") {
+          xml.map {
+            xml.send(:"concept-list") {
+              xml.concept("label" => "node1", "id" => "idnode1")
+              xml.concept("label" => "node2", "id" => "idnode2")
+            }
+            xml.send(:"concept-appearance-list") {
+              xml.send(:"concept-appearance", "id" => "idnode1", "x" => "99", "y" => "99", "width" => "53", "height" => "25")
+              xml.send(:"concept-appearance", "id" => "idnode2", "x" => "1234", "y" => "1234", "width" => "53", "height" => "25")
+            }
+          }
+        }
+    end
+    
+    cmap = CMap::CMap.new(test.doc)
+    key = CMap::CMap.new(correct.doc)
+    cmap.move_nodes key
+    
+    xml = correct.doc
+    assert_equal(xml.xpath("//concept-appearance-list/concept-appearance"), cmap.instance_variable_get(:@xml).xpath("//concept-appearance-list/concept-appearance"))
+  end
+  
+  def test_no_appear_mult_node
     test = Builder.new do |xml|
         xml.cmap("xmlns" => "http://cmap.ihmc.us/xml/cmap/") {
           xml.map {
