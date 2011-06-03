70a9f7e9b840e6c26ede697de42fdf899e24cc99
Trebor Shankle <trebor.shankle@gmail.com>
2011-06-03 02:19:20 +0000

    Cleaned-up the way rdoc is generated, so that files are no longer generated for tests.
    --Trebor Shankle

--- src/cmap-eval.rb
+++ src/cmap-eval.rb
@@ -1,3 +1,6 @@
+# This is where execution begins in cmap-eval.  cmap-eval expects to be called from the command line, and uses several
+# helper files, notably c_map.rb, to grade and mark cmaps.  Execution begins with a call to execute, which might call
+# any of several local methods depending on the execution mode.
 
 # Fix the search path so that all of our relative includes work.
 $: << File.expand_path(File.dirname(__FILE__) + "/..")
@@ -57,29 +60,35 @@ def batch_mode key_file_name, batch_path
   end
 end
 
-# Validate the arguments and get the mode the program should run in.
-Arguments_Helper.validate_arguments ARGV
-mode = Arguments_Helper.get_mode ARGV
-
-if mode == :debug_mode
-  Debug.enable_debug
+# Perform the main behaviors of cmap-eval; execute in :debug_mode, :problem_statement_mode, :batch_mode, or :normal_mode
+# as needed.
+def execute
+  # Validate the arguments and get the mode the program should run in.
+  Arguments_Helper.validate_arguments ARGV
+  mode = Arguments_Helper.get_mode ARGV
+  
+  if mode == :debug_mode
+    Debug.enable_debug
+  end
+  
+  # Check the mode and react accordingly.
+  if mode == :problem_statement_mode
+    key_file_name, output_path = Arguments_Helper.get_problem_statement_names ARGV
+    Arguments_Helper.validate_readable key_file_name
+    
+    problem_map = CMap::CMap.new Nokogiri::XML File.read key_file_name
+    
+    # create the problem statement CMap with nodes
+    problem_map.transform_into_problem_statement_map
+    problem_map.write_to_file output_path + "/problem_statement.cxl"
+  elsif mode == :batch_mode
+    is_batch_mode
+    key_file_name, batch_path = Arguments_Helper.get_batch_file_names ARGV
+    batch_mode key_file_name, batch_path
+  else
+    key_file_name, input_file_name = Arguments_Helper.get_normal_file_names ARGV
+    normal_mode key_file_name, input_file_name
+  end
 end
 
-# Check the mode and react accordingly.
-if mode == :problem_statement_mode
-  key_file_name, output_path = Arguments_Helper.get_problem_statement_names ARGV
-  Arguments_Helper.validate_readable key_file_name
-  
-  problem_map = CMap::CMap.new Nokogiri::XML File.read key_file_name
-  
-  # create the problem statement CMap with nodes
-  problem_map.transform_into_problem_statement_map
-  problem_map.write_to_file output_path + "/problem_statement.cxl"
-elsif mode == :batch_mode
-  is_batch_mode
-  key_file_name, batch_path = Arguments_Helper.get_batch_file_names ARGV
-  batch_mode key_file_name, batch_path
-else
-  key_file_name, input_file_name = Arguments_Helper.get_normal_file_names ARGV
-  normal_mode key_file_name, input_file_name
-end
+execute
